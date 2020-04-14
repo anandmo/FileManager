85609eb96a57ad04cb30a3fa356f0cf23ea3f89e
Anand Mohan <im.anand.m@gmail.com>
2020-04-14 16:51:55 +0530

    Created File Model class

---
+++ src/com/lokedme/model/LMFile.java
@@ -0,0 +1,56 @@
+package com.lokedme.model;
+
+import java.time.LocalDateTime;
+
+public class LMFile {
+
+    private LocalDateTime fileCreationTime, lastModificationTime;
+    private String fileType, fileName, filePath;
+    private double fileSize;
+
+    public LocalDateTime getFileCreationTime() {
+        return fileCreationTime;
+    }
+
+    public void setFileCreationTime(LocalDateTime fileCreationTime) {
+        this.fileCreationTime = fileCreationTime;
+    }
+
+    public LocalDateTime getLastModificationTime() {
+        return lastModificationTime;
+    }
+
+    public void setLastModificationTime(LocalDateTime lastModificationTime) {
+        this.lastModificationTime = lastModificationTime;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public double getFileSize() {
+        return fileSize;
+    }
+
+
+}
