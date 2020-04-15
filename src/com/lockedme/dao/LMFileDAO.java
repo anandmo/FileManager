589ed0a08f981e7f3a996c67733dd6298e1c8670
Anand Mohan <im.anand.m@gmail.com>
2020-04-15 17:19:24 +0530

    Added abstract method to DAO interface

--- src/com/lockedme/dao/LMFileDAO.java
+++ src/com/lockedme/dao/LMFileDAO.java
@@ -1,5 +1,13 @@
 package com.lockedme.dao;
 
+import com.lockedme.model.LMFile;
+
 public interface LMFileDAO {
+
+boolean addLMFiletToStorage(LMFile lmFile);
+boolean findLMFileFromStorage(String fileName);
+boolean deleteLMFileFromStorage(String fileName);
+void displayAllLMFileFromStorage();
+
 
 }
