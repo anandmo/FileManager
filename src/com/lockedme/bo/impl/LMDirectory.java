9c9c529dc6f6025c7a32ea942a8ee9663dea2b45
Anand Mohan <im.anand.m@gmail.com>
2020-04-15 15:49:19 +0530

    Added LMDirectory class to create directory and userInterface

--- src/com/lockedme/bo/impl/LMDirectory.java
+++ src/com/lockedme/bo/impl/LMDirectory.java
@@ -1,4 +1,24 @@
 package com.lockedme.bo.impl;
 
+import java.io.File;
+
 public class LMDirectory {
+
+    String fileSeparator = System.getProperty("file.separator");
+    String root = "C:"+fileSeparator+"FileManager"+fileSeparator+"root";
+
+    public  boolean createRootDirectory() {
+        File file = new File(root);
+        return file.mkdir();
+    }
+
+    public boolean createRootDirectory(String drive,String folderName) {
+
+        String customPath = drive+fileSeparator+folderName;
+        File file = new File(customPath);
+
+        return file.mkdir();
+
+    }
+
 }
---
+++ src/com/lockedme/userinferface/Main.java
@@ -0,0 +1,13 @@
+package com.lockedme.userinferface;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] a = {"qww","sdsd"};
+
+        System.out.println(a.toString());
+
+    }
+
+}
